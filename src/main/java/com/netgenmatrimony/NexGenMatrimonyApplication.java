ef7aba1934e08450f1b6517adfd3d4d5d0384ccd
srukmagathan <srukmagathan@bridge2solutions.com>
2018-04-11 20:14:30 +0530

    initial commit

---
+++ src/main/java/com/netgenmatrimony/NexGenMatrimonyApplication.java
@@ -0,0 +1,12 @@
+package com.netgenmatrimony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexGenMatrimonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexGenMatrimonyApplication.class, args);
+	}
+}
